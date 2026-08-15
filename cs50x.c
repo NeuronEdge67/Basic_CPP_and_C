76ddea4b3a227f9ac34202c3a4ec9c105974f8ed
Akhilesh <akhileshakhilesh2880@gmail.com>
2026-08-15 21:48:25 +0530

    cs50x

--- cs50x.c
+++ cs50x.c
@@ -2,5 +2,22 @@
 
 int main(void)
 {
-    printf("fnefnewwwj");
+    int x = get_int("What's X? ")
+    int y = get_int("What's Y? ")
+
+    if (x > y)
+    {
+        printf("X is grater than Y");
+    }
+
+    else if (x < y)
+    {
+        printf("X is smaller than Y");
+    }
+
+    else (x == y)
+    {
+        printf("X is equal to Y");
+    }
 }
+

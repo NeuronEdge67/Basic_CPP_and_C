dbd669927266117eeceb787afa4d9ad438886129
Akhilesh <akhileshakhilesh2880@gmail.com>
2026-08-17 21:37:25 +0530

    variables & while loop in C

--- tutorial_intro.c
+++ tutorial_intro.c
@@ -3,4 +3,18 @@
 int main(void)
 {
     printf("Hello, World!");
+
+    int fahr, celcius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper){
+        celcius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celcius);
+        fahr += step;
+    }
 }

580b46541c26f8bf01ff382e87017c0ea8a8db48
akhilesh2009 <akhileshakhilesh2880@gmail.com>
2026-08-21 22:06:34 +0530

    boolean in c

--- variables_and_statements.c
+++ variables_and_statements.c
@@ -1,10 +1,16 @@
 # include <stdio.h>
+# include <stdbool.h>
 
 int main(void)
 {
     int i = 67;
     float f = 3.14;
     char *s = "Hello";
+    bool x = true;
+
+    if (x){
+        printf("x is true\n");
+    }
 
     printf("%s %d %f", s, i, f);
 }

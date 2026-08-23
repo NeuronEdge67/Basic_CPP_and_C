e892c19e69b0c1b852c87a18327ecfcf07c78417
akhilesh2009 <akhileshakhilesh2880@gmail.com>
2026-08-23 21:43:08 +0530

    while loop in C

--- flow_control.c
+++ flow_control.c
@@ -2,15 +2,23 @@
 
 int main(void)
 {
-    int i = 11;
+    int i = 11, x;
 
     if (i == 69) {
         printf("I is %d", i);
     }
 
     else {
-        printf("I is not 6. I is %d", i);
+        printf("I is not 6. I is %d\n", i);
     }
+
+    x = 0;
+
+    while (x < 10){
+        printf("x is %d now\n", x);
+        x++;
+    }
+    printf("All done!\n");
 
     // y += x > 10? 17: 37;
 

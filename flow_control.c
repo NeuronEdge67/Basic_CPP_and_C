92aa3f2cdd2a30ae65026ac5887e86d5d9ffc995
akhilesh2009 <akhileshakhilesh2880@gmail.com>
2026-08-23 21:57:57 +0530

    while and do-while

--- flow_control.c
+++ flow_control.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    int a = 11, x;
+    int a = 11, x, i;
 
     if (a == 69) {
         printf("I is %d", a);
@@ -20,7 +20,33 @@ int main(void)
     }
     printf("All done!\n");
 
+    i = 10;
+while(i < 10) {
+printf("while: i is %d\n", i);
+i++;
+}
+i = 10;
+do {
+printf("do-while: i is %d\n", i);
+i++;
+} while (i < 10);
+printf("All done!\n");
+
+
     // y += x > 10? 17: 37;
+
+    i = 10;
+    while(i < 10) {
+    printf("while: i is %d\n", i);
+    i++;
+    }
+    do {
+    i = 10;
+    printf("do-while: i is %d\n", i);
+    i++;
+    } while (i < 10);
+    printf("All done!\n");
+
 
     return 0;
 }

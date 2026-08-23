af945d1087d8d88098e58c1c7199387655470a06
akhilesh2009 <akhileshakhilesh2880@gmail.com>
2026-08-23 21:18:44 +0530

    if in c

---
+++ flow_control.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main(void)
+{
+    int i = 67;
+
+    if (i == 69) {
+        printf("I is %d", i);
+    }
+
+    else {
+        printf("I is not 69. I is %d", i);
+    }
+}

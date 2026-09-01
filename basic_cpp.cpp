a14c80d25f922f24a5093cdb90566741c40e88b3
akhilesh2009 <akhileshakhilesh2880@gmail.com>
2026-09-01 21:51:48 +0530

    variables in cpp

--- basic_cpp.cpp
+++ basic_cpp.cpp
@@ -5,7 +5,7 @@ int main()
     std::cout << "Hello, World!\n";
     std::cout << "H\n";
     std::cout << "Hello\n";
-    std::cout << 67;
+    std::cout << 67 <<"\n";
     int  x {5};
     std::cout << x;
 

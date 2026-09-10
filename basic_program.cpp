86c755fd34525f7cc70dea4bfcd305f3e4fe4b2d
akhilesh2009 <akhileshakhilesh2880@gmail.com>
2026-09-10 22:00:07 +0530

    bool in cpp

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
 
-double get_user_input()
-{
-    std::cout << "Enter a Number: ";
-    double value;
-    std::cin >> value;
-    return value;
+bool is_equal(int x, int y){
+    return x == y;
 }
+
+// double get_user_input()
+// {
+//     std::cout << "Enter a Number: ";
+//     double value;
+//     std::cin >> value;
+//     return value;
+// }
 
 int main()
 {
-    double num = get_user_input();
 
-    std::cout << "Double of " << num << " is " << num * 2;
+    std::cout << "Enter a integer: ";
+    int x{};
+    std::cin >> x;
+
+    std::cout << "Enter another integer: ";
+    int y{};
+    std::cin >> y;
+
+    std::cout << std::boolalpha;
+
+    std::cout << "Is " << x << " and " << y << " equal? ";
+    std::cout << is_equal(x, y) << "\n";
+
+
+    // double num;
+    
+    // num = get_user_input();
+
+    // std::cout << "Double of " << num << " is " << num * 2;
     
     return 0;
 }
---
+++ calculator.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Calculator CPP";
+    return 0;
+}
